2257f79aaff06cd5806da63497f69d27794fdb4d
Calvin Pang <cal.pang123@gmail.com>
2024-02-01 18:20:58 +1100

    Create some utility function

---
+++ src/main/java/com/medium/mediumauthcommons/AuthUtility.java
@@ -0,0 +1,10 @@
+package com.medium.mediumauthcommons;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthUtility {
+    public boolean isAuthenticated() {
+        return true;
+    }
+}
